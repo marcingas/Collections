cf2d0ff5879db2a6eb4496f810d470be4c4d3510
Marcin <gasiorek.mail@gmail.com>
2023-03-30 09:46:54 +0200

    clear unused data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("======================================================================");
     }
 }
+
